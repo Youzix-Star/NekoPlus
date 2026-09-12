bc4fbf8aaeac05bd1a53b2fdb2d019f225eb7844
dev <dev@local>
2026-09-13 05:07:45 +0800

    fix: MD 外壳补 dp 导入

--- app/src/main/java/love/miao/yun/ui/material3/MaterialApp.kt
+++ app/src/main/java/love/miao/yun/ui/material3/MaterialApp.kt
@@ -44,6 +44,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import love.miao.yun.MiaoState
 import love.miao.yun.service.FloatingWindowService
