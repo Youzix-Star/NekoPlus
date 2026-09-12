93f38589430a05181dac2bfa7227e1b3c65af28f
dev <dev@local>
2026-09-13 04:53:43 +0800

    fix: MD 关于页补上 remember 导入

--- app/src/main/java/love/miao/yun/ui/material3/about/MaterialAboutScreen.kt
+++ app/src/main/java/love/miao/yun/ui/material3/about/MaterialAboutScreen.kt
@@ -49,6 +49,7 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
